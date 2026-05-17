b6515c99b64a0eb2184f6ce4a080679a008c282b
nhh-7 <u_nhh7@163.com>
2026-05-17 22:11:36 +0800

    数据集移动命令

---
+++ dataset_load.sh
@@ -0,0 +1,9 @@
+oss cp oss://datasets/DIV2K_train_HR.zip /hy-tmp
+
+oss cp oss://datasets/DIV2K_train_LR_bicubic_X2.zip /hy-tmp
+
+oss cp oss://datasets/DIV2K_train_LR_bicubic_X3.zip /hy-tmp
+
+oss cp oss://datasets/DIV2K_train_LR_bicubic_X4.zip /hy-tmp
+
+oss cp oss://datasets/TestDataSR.zip /hy-tmp
