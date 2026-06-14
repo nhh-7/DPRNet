faa09ff62c896ee3ea73b0e92d3adba9651bc8e8
huanghainan <u_nhh7@163.com>
2026-06-14 16:39:22 +0800

    feat：修改训练后上传关机逻辑

---
+++ CATANet/run_x4_ablation_remaining.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nnodes=1 --nproc_per_node=4 \
+  basicsr/train.py -opt options/train/train_CATANet_x4_abl_A1_refine_off.yml --launcher pytorch
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nnodes=1 --nproc_per_node=4 \
+  basicsr/train.py -opt options/train/train_CATANet_x4_abl_A2_v1_hardsort.yml --launcher pytorch
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nnodes=1 --nproc_per_node=4 \
+  basicsr/train.py -opt options/train/train_CATANet_x4_abl_A2_v2_confsort.yml --launcher pytorch
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nnodes=1 --nproc_per_node=4 \
+  basicsr/train.py -opt options/train/train_CATANet_x4_abl_A2_v3_scoregate.yml --launcher pytorch
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nnodes=1 --nproc_per_node=4 \
+  basicsr/train.py -opt options/train/train_CATANet_x4_abl_A3_balance_off.yml --launcher pytorch
+
+bash backup_result_and_shutdown.sh
